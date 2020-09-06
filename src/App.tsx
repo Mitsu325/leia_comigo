0e5bb8783d22185a7d312cd3061fbe68faa6e9df
Mitsu325 <patricia.mashiba@gmail.com>
2020-09-06 20:38:52 -0300

    Create basic structure

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <h1>Leia comigo</h1>
+      <GlobalStyle />
+    </>
+  );
+};
+
+export default App;
---
+++ src/styles/global.ts
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #FFF;
+    color: #58595B;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font: 18px 'Roboto', sans-serif;
+  }
+
+	h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 700;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
